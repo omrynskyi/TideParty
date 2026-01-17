e213a5a98a075d3439b53fca2daaa494f9a76cc8
Oleg Mrynskyi <omrynskyi@gmail.com>
2026-01-16 19:17:31 -0800

    Initial Commit

---
+++ TideParty/TidePartyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TidePartyApp.swift
+//  TideParty
+//
+//  Created by Oleg Mrynskyi on 1/16/26.
+//
+
+import SwiftUI
+
+@main
+struct TidePartyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
